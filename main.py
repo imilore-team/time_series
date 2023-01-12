996143db2d2aa240e34ce02bd71b91f8c7e68c97
boldirev-as <wot.1106@yandex.ru>
2023-01-12 16:36:03 +0300

    new data for one metric

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import sqlite3
+from tqdm import tqdm
 
 conn = sqlite3.connect('new_data.sql')
 cursor = conn.cursor()
 todo = []
-for row in open('data-dump.sql', mode="r"):
+for row in tqdm(open('data-dump.sql', mode="r", encoding="utf8", errors="ignore")):
     if not row.startswith('--'):
         todo.append(row)
-new_todo = "".join(todo).replace("\n", "").split(";")
-open("out.txt").writelines(new_todo)
+new_todo = "".join(todo).replace("\n", "").replace("`", '"').replace("''", "").split(";")
+open("out.txt", mode="w", errors="ignore").writelines(new_todo)
 
-for row in new_todo:
-    cursor.execute(row)
+for row in tqdm(new_todo):
+    try:
+        cursor.execute(row)
+    except Exception as e:
+        print(row)
+        print(e)
+        break
 
 print(cursor.fetchone()[0])
